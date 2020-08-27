281141dcd35ce1dc870e05e087a2df4cc5ae52e8
Benoit Bouthillier <benoit.bouthillier@montreal.ca>
2020-08-27 19:20:24 -0400

    Updated ansible-install - UTM v0.4

--- ansible-install/_install.sh
+++ ansible-install/_install.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-
-# Get UTM from github
-git clone https://github.com/paregupt/ucs_traffic_monitor
-
-
-# Prepare updated version of grafana dashboard if using vagrant (port 80 is not available, will use 8080 instead)
-rm -Rf /tmp/utm
-mkdir -p /tmp/utm
-cp ucs_traffic_monitor/grafana/dashboards/* /tmp/utm
-sed -i 's;window.location.hostname + \\"/cisco_logo.png;window.location.hostname + \\":8080/cisco_logo.png;g' /tmp/utm/*  
-sed -i 's;window.location.hostname + \\"/world_map.png;window.location.hostname + \\":8080/world_map.png;g'   /tmp/utm/*  
-sed -i 's;window.location.hostname + \\"/your_logo.png;window.location.hostname + \\":8080/your_logo.png;g'   /tmp/utm/*  
-
-
-# Start vagrant VM and configure
+# Start vagrant VM
 vagrant up
-ansible-playbook -i utm.vagrant prereq.yml
-ansible-playbook -i utm.vagrant utm.yml --extra-vars "alternate_grafana_dashboard=/tmp/utm"
+
+# Install community.grafana
+ansible-galaxy collection install community.grafana
+
+# Test ansible ssh connection
+ansible -i inventory all -m shell -a "uptime"
+
+# Perform installation (must be run from ansible-install folder)
+ansible-playbook -i inventory prereq.yml
+ansible-playbook -i inventory utm.yml
